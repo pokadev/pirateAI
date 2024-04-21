e966021e534014db2315452a97bbc835b0c81ce4
dmitry <poka@MBP.local>
2024-04-21 23:04:30 +0300

    implement in main.go openai

--- go.mod
+++ go.mod
@@ -4,5 +4,11 @@ go 1.22
 
 require (
 	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/henomis/lingoose v0.1.2 // indirect
+	github.com/iancoleman/orderedmap v0.0.0-20190318233801-ac98e3ecb4b0 // indirect
+	github.com/invopop/jsonschema v0.7.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/sashabaranov/go-openai v1.19.2 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
+	"github.com/henomis/lingoose/llm/openai"
+	"github.com/henomis/lingoose/thread"
 	"os"
 )
 
@@ -12,4 +16,30 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter text: ")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+
+	myThread := thread.New().AddMessages(
+		thread.NewSystemMessage().AddContent(
+			thread.NewTextContent("All replies must be given in a pirate style of speech"),
+		),
+		thread.NewUserMessage().AddContent(
+			thread.NewTextContent(text),
+		),
+	)
+
+	err = openai.New().Generate(context.Background(), myThread)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Error: Something went wrong. Please check your API KEY & account")
+		os.Exit(1)
+	}
+
+	fmt.Println("Pirate :" + myThread.LastMessage().Contents[0].AsString())
+	os.Exit(0)
 }
